2a581e3ec22a20d5e4d1a7c1892f1645b65b9d08
ushakumarik <k.ushakumari@yahoo.com>
2018-03-31 15:13:26 +0530

    Adding watch to package json

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import LikeMe from "./LikeMe.jsx";
 
 class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
+  render() {
+    return (
+      <div>
+        <p> Hello React!</p>
+        <LikeMe />
+      </div>
+    );
   }
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App />, document.getElementById("app"));
